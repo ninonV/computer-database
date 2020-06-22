7724c9d211a9d5c116136df36db5e61f20d75e08
Ninon Vrignaud <ninonvrignaud@hotmail.com>
2020-06-22 18:12:12 +0200

    Logger + Begin Tests

---
+++ src/test/java/com/excilys/java/mapper/CompanyMapperTest.java
@@ -0,0 +1,50 @@
+package com.excilys.java.mapper;
+
+import static org.junit.Assert.*;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+
+import com.excilys.java.model.Company;
+
+public class CompanyMapperTest {
+	
+	private static final String ID_COMPANY = "id";
+    private static final String NAME_COMPANY = "name";
+	
+	private final Long id = 10L;
+	private final String name = "Company test name";
+	
+	private ResultSet resultSet = Mockito.mock(ResultSet.class);
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testMap() {
+		try {
+			Mockito.when(resultSet.getLong(ID_COMPANY)).thenReturn(id);
+			Mockito.when(resultSet.getString(NAME_COMPANY)).thenReturn(name);
+		} catch (SQLException e) {
+			fail("Fail to construct ResultSet");
+		}
+		
+		Company companyMapper = CompanyMapper.map(resultSet);
+		Company company = new Company(id,name);
+		
+		assertEquals(company.getIdCompany(), companyMapper.getIdCompany());
+		assertEquals(company.getName(), companyMapper.getName());
+		
+	}
+
+
+}
---
+++ src/test/java/com/excilys/java/model/PageTest.java
@@ -0,0 +1,24 @@
+package com.excilys.java.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class PageTest {
+	
+	Page page = Mockito.mock(Page.class);
+	
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
