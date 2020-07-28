9ae0f8a8d533dd7437841794f72a50c090c39546
Ninon Vrignaud <ninonvrignaud@hotmail.com>
2020-07-28 18:37:14 +0200

    Spring security BUG

--- webapp/src/main/java/com/excilys/java/CDB/configuration/SpringSecurityConfiguration.java
+++ webapp/src/main/java/com/excilys/java/CDB/configuration/SpringSecurityConfiguration.java
@@ -2,18 +2,14 @@ package com.excilys.java.CDB.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.www.DigestAuthenticationEntryPoint;
-import org.springframework.security.web.authentication.www.DigestAuthenticationFilter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -22,52 +18,22 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	UserDetailsService userService;
 
-	@Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-	
 	@Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	http.authorizeRequests()
-            .antMatchers("/login").permitAll()
-            .antMatchers("/RegisterUser").permitAll()
-            .antMatchers(HttpMethod.GET, "/", "/ListComputer/**").hasAnyRole("USER", "ADMIN")
-            .antMatchers(HttpMethod.POST, "/", "/ListComputer/**").hasRole("ADMIN")
-            .antMatchers("/AddComputer/**").hasRole("ADMIN")
-            .antMatchers("/EditComputer/**").hasRole("ADMIN")
-            .and().formLogin().loginPage("/login").defaultSuccessUrl("/ListComputer").failureUrl("/login?error=true").permitAll()
-            .and().logout()
-            .and().csrf().disable();
-    		//.addFilter(digestAuthenticationFilter());
-    }
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/login", "/resources/**").permitAll()
+		.and().formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password").defaultSuccessUrl("/ListComputer").permitAll()
+		.and().logout().logoutUrl("/logout").clearAuthentication(true)
+		.and().authorizeRequests().anyRequest().authenticated();
+	}
 
-	/*DigestAuthenticationFilter digestAuthenticationFilter() throws Exception {
-		DigestAuthenticationFilter digestAuthenticationFilter = new DigestAuthenticationFilter();
-		digestAuthenticationFilter.setUserDetailsService(userService);
-		digestAuthenticationFilter.setAuthenticationEntryPoint(digestEntryPoint());
-		return digestAuthenticationFilter;
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
 	}
 
 	@Bean
-	DigestAuthenticationEntryPoint digestEntryPoint() {
-		DigestAuthenticationEntryPoint bauth = new DigestAuthenticationEntryPoint();
-		bauth.setRealmName("Digest Authentication");
-		bauth.setKey("MySecureKey");
-		return bauth;
-	}*/
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder authManagerBuilder) throws Exception {
-		//authManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
-		authManagerBuilder.userDetailsService(userService);
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
-	
-	@Bean
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-	
+
 }
